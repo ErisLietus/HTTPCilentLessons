80afc45642fb54a68cd2d92a2d4bf08b9fb58cbd
Eris Lietus <yukitoichijo@gmail.com>
2026-05-08 14:11:15 +0100

    admin

--- dist/index.js
+++ dist/index.js
@@ -6,11 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const readiness_1 = require("./api/readiness");
 const middleware_1 = require("./api/middleware");
+const metrics_1 = require("./api/metrics");
+const reset_1 = require("./api/reset");
 const app = (0, express_1.default)();
 const PORT = 8080;
 app.use(middleware_1.middlewareLogResponses);
-app.get("/healthz", readiness_1.handlerReadiness);
-app.use("/app", express_1.default.static("./src/app"));
+app.get("/api/healthz", readiness_1.handlerReadiness);
+app.get("/admin/metrics", metrics_1.handlerMetrics);
+app.get("/admin/reset", reset_1.handlerReset);
+app.use("/app", middleware_1.middlewareMetricsInc, express_1.default.static("./src/app"));
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/api/metrics.ts
+++ src/api/metrics.ts
@@ -2,6 +2,11 @@ import { Request, Response } from "express";
 import { config } from "../config";
 
 export async function handlerMetrics(req: Request,  res:Response): Promise<void>{
-    res.set("Content-Type", "text/plain; charset=utf-8")
-    res.send(`Hits: ${config.fileserverHits}`)
+    res.set("Content-Type", "text/html; charset=utf-8")
+    res.send(`<html>\n
+        <body>\n
+            <h1>Welcome, Chirpy Admin</h1>\n
+            <p>Chirpy has been visited ${config.fileserverHits} times!</p>\n
+        </body>\n
+    </html>`)
 }
